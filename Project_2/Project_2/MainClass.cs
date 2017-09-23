c81c53ba54ac43d76623f4211a168f25ec42b77c
Conor Yates-Koch <cyatesko@asu.edu>
2017-09-23 13:23:22 -0700

    Ruining everything

--- Project_2/Project_2/MainClass.cs
+++ Project_2/Project_2/MainClass.cs
@@ -13,20 +13,28 @@ namespace WeimoPlant
         static void Main(string[] args)
         {
             //Create a new plant object
-            Plant carPlant = new Plant();
+            Plant carPlant1 = new Plant();
+            Plant carPlant2 = new Plant();
+            Plant carPlant3 = new Plant();
 
-            //Create (3) car manufacturers
-            Thread[] plants = new Thread[3];
-            for (int i = 0; i < 3; i++) {
-                plants[i] = new Thread(new ThreadStart(carPlant.plantFunc));   //Multithread the plantFunc() section of Plant.cs
-                plants[i].Name = (i + 1).ToString();                           //Name each thread
-                plants[i].Start();                                             //Start each thread
-            }
+            Thread plant1 = new Thread(new ThreadStart(carPlant1.plantFunc));
+            Thread plant2 = new Thread(new ThreadStart(carPlant2.plantFunc));
+            Thread plant3 = new Thread(new ThreadStart(carPlant3.plantFunc));
+
+            plant1.Name = "Plant 1";
+            plant2.Name = "Plant 2";
+            plant3.Name = "Plant 3";
+
+            plant1.Start();
+            plant2.Start();
+            plant3.Start();
 
             //Create a test dealer
             Dealer dealership = new Dealer();
             //Subscribe dealer to the price cut event
-            Plant.priceCut += new priceCutEvent(dealership.carsOnSale);
+            carPlant1.priceCut += new priceCutEvent(dealership.carsOnSale);
+            carPlant2.priceCut += new priceCutEvent(dealership.carsOnSale);
+            carPlant3.priceCut += new priceCutEvent(dealership.carsOnSale);
 
             //Create (3) car dealerships
             Thread[] dealerships = new Thread[3];
